1cd6786580cefd3c98e07fb5b291b442077ac058
DhestaLingga06 <dhestalingga21@gmail.com>
2025-10-22 14:14:10 +0700

    Change 1

--- SiakadFor06.java
+++ SiakadFor06.java
@@ -4,6 +4,8 @@ public class SiakadFor06 {
         Scanner sc = new Scanner(System.in);
 
     double nilai, tertinggi = 0, terendah = 100;
+    int lulus = 0, tidakLulus = 0;
+    
     for (int i = 1; i <= 10; i++) {
         System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
         nilai = sc.nextDouble();
@@ -13,10 +15,18 @@ public class SiakadFor06 {
         if (nilai < terendah) {
             terendah = nilai;
         }
+
+        if (nilai >= 60) {
+            lulus++;
+        } else {
+            tidakLulus++;
+        }
     }
     
     System.out.println("Nilai tertinggi: " + tertinggi);
     System.out.println("Nilai terendah: " + terendah);
+    System.out.println("Jumlah mahasiswa lulus: " + lulus);
+    System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
 
     sc.close();
     
